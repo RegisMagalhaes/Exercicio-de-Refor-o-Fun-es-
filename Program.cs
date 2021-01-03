16bc5865fadfd7eab75083fc758c0a1e94c4dde1
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2021-01-02 22:11:15 -0300

    Este programa se refere a aula de Funções

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Usando_Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float a, b;
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Digite um valor: ");
+            a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro valor: ");
+            b = float.Parse(Console.ReadLine());
+
+            Somar(a,b);
+
+            Subtrair(a,b);
+
+            Multiplicar(a,b);
+        }
+
+        static void Somar(float a, float b){
+
+            Console.WriteLine(a+b);
+
+            
+        }
+        static void Subtrair(float a, float b){
+
+            Console.WriteLine(a-b);
+
+            
+        }
+        static void Multiplicar(float a, float b){
+
+            Console.WriteLine(a*b);
+
+            
+        }
+    }    
+
+}
+
+
+
+
+
+        
